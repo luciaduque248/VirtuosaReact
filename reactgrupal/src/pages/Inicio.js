f3208b2ed269017ce23a31b52b7a2baf4815c638
luciaduque248 <luciaduque248@gmail.com>
2022-05-25 08:55:41 -0500

    header and footer  was modified, index was added

--- reactgrupal/src/pages/Inicio.js
+++ reactgrupal/src/pages/Inicio.js
@@ -1,25 +1,67 @@
 import Header from '../components/header/header';
 import Footer from '../components/footer/footer';
 import Home from '../components/home/home';  // nav bar
+import { Link } from 'react-router-dom';
+
+import '../components/assets/css/inicio.css'
 
 function Inicio() {
     return (
         <div>
             <Header />
             <Home />
-            <div className="section">
-                <div className="text1">
-                    <h1>Diseñadora</h1>
-                    <h2>Hola, mi nombre es</h2>
-                    <h2 className="line2 line2-sara">Sara Duque</h2>
-                </div>
-                <div className="text2">
-                    <h1>Frontend</h1>
-                    <h2>Desarrolladora quien se</h2>
-                    <h2 className="line2 line-frontend2">enfoca en codigo limpio</h2>
-                    <h2 className="line3 line-frontend3">y eficiente</h2>
-                </div>
+            <section class="contenido">
+        <div class="maquillaje">
+            <h1>DEFINE TU BELLEZA</h1>
+            <button><Link to="*" className="active item">Ver más</Link></button>
+        </div>
+        <div class="ropa">
+            <h1>DISEÑOS EXCLUSIVOS</h1>
+            <button><Link to="/moda" className="active item">Ver más</Link></button>
+        </div>
+    </section>
+
+    <section class="rosa">
+        <div class="rose">
+            <div class="iconoRose">
+                <i class="fa-solid fa-box-open fa-lg"></i>
             </div>
+            <div class="contentRose">
+                <h3>ENVIO GRATIS</h3>
+                <p>DESDE $200.000</p>
+            </div>
+        </div>
+
+        <div class="rose">
+            <div class="iconoRose">
+                <i class="fa-solid fa-bag-shopping fa-lg"></i>
+            </div>
+            <div class="contentRose">
+                <h3>COMPRA 100%</h3>
+                <h4>SEGURA</h4>
+            </div>
+        </div>
+
+        <div class="rose">
+            <div class="iconoRose">
+                <i class="fa-solid fa-credit-card fa-lg"></i>
+            </div>
+            <div class="contentRose">
+                <h3>MULTIPLES MEDIOS</h3>
+                <p>DE PAGO</p>
+            </div>
+        </div>
+
+        <div class="rose">
+            <div class="iconoRose">
+                <i class="fa-solid fa-cash-register fa-lg"></i>
+            </div>
+            <div class="contentRose">
+                <h3>DEVOLUCIONES</h3>
+                <h4>SIN COSTO</h4>
+            </div>
+        </div>
+    </section>
             <Footer/>
         </div>
     )
